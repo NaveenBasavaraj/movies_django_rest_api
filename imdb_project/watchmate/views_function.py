8867c8da94a8899b971ae3e57b977139d02c2432
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2024-08-04 08:42:28 +0000

    added different http methods

--- imdb_project/watchmate/views_function.py
+++ imdb_project/watchmate/views_function.py
@@ -4,39 +4,39 @@ from rest_framework.decorators import api_view
 from watchmate.api.serializers import MovieSerializer, StreamSerializer, ReviewSerializer
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def movie_list_api_view(request):
     movies = Movie.objects.all()
     serializer = MovieSerializer(movies, many=True)
     return Response(serializer.data)
 
-@api_view()
+@api_view(['GET','PUT','DELETE'])
 def movie_detail_api_view(request, pk):
     movie = Movie.objects.get(pk=pk)
     serializer = MovieSerializer(movie)
     return Response(serializer.data)
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def review_list_api_view(request):
     reviews = Review.objects.all()
     serializer = ReviewSerializer(reviews, many=True)
     return Response(serializer.data)
 
-@api_view()
+@api_view(['GET','PUT','DELETE'])
 def review_detail_api_view(request, pk):
     review = Review.objects.get(pk=pk)
     serializer = ReviewSerializer(review)
     return Response(serializer.data)
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def streaming_platform_list_api_view(request):
     sps = SteamingPlatform.objects.all()
     serializer = StreamSerializer(sps, many=True)
     return Response(serializer.data)
 
-@api_view()
+@api_view(['GET','PUT','DELETE'])
 def streaming_platform_detail_api_view(request, pk):
     sp = SteamingPlatform.objects.get(pk=pk)
     serializer = StreamSerializer(sp)
